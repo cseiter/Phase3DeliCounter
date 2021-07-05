01cd4940f0352091de2fdb9f842e0642ea03a6ab
chris <cseiter@gmail.com>
2021-07-05 16:38:39 -0400

    line method passes tests

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(katz_deli)
+    if katz_deli.count > 0 then
+        line_array = []
+        katz_deli.map.with_index(1) {|cust_name,line_place| line_array << "#{line_place}. #{cust_name}"}
+        line_string = line_array.join(" ")
+        puts "The line is currently: #{line_string}"
+    else
+        puts "The line is currently empty." 
+    end
+end
+
+line(["anne","wil"])
